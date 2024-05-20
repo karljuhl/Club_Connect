4657a93434e83b4ef2cd522846b8e68afc5c1cf3
seb-AA <seb@asgeranalytics.com>
2024-05-20 15:34:15 -0600

    site config

--- config/site.ts
+++ config/site.ts
@@ -4,7 +4,7 @@ export const siteConfig: SiteConfig = {
   name: "ClubConnect",
   description:
     "A Platform to Enable your Team.",
-  url: "http://localhost:3000/",
+  url: "https://club-connect-three.vercel.app",
   ogImage: "https://www.openassistantgpt.io/dashboard.png",
   links: {
     twitter: "https://twitter.com/clubconnect",
