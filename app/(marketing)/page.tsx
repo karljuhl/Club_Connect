ca2f95b30f29596b1032a690de7ddf29b1b837fe
seb-AA <seb@asgeranalytics.com>
2024-05-22 09:44:56 -0600

    homepg styling 6

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -179,7 +179,7 @@ export default function IndexPage() {
             </div>
         </div>
         <div className="mx-auto mt-10 grid max-w-sm items-start gap-12 sm:max-w-4xl sm:grid-cols-2 lg:max-w-5xl lg:grid-cols-4">
-            <div className="relative flex flex-col space-y-2 border px-3 py-3 rounded-md">
+            <div className="relative flex flex-col space-y-2 border px-3 py-3 rounded-md bg-gray-100">
                 <div className="space-y-2">
                     <h3 className="text-2xl font-bold tracking-tighter">{hobbyPlan.name}</h3>
                     <p className="text-2xl font-bold tracking-tighter">${hobbyPlan.price}</p>
@@ -193,10 +193,10 @@ export default function IndexPage() {
                     <li><Icons.check className="mr-2 inline-block h-4 w-4" /> Unlimited Messages</li>
                 </ul>
             </div>
-            <div className="relative flex flex-col space-y-2 border px-3 py-3 rounded-md">
+            <div className="relative flex flex-col space-y-2 border px-3 py-3 rounded-md bg-gray-100">
                 <div className="space-y-2">
-                    <h3 className="text-2xl font-bold tracking-tighter text-gray-200">{managedWeb.name}</h3>
-                    <p className="text-2xl font-bold tracking-tighter text-gray-200">${managedWeb.price}</p>
+                    <h3 className="text-2xl font-bold tracking-tighter">{managedWeb.name}</h3>
+                    <p className="text-2xl font-bold tracking-tighter">${managedWeb.price}</p>
                     <p className="text-sm text-gray-700">For modern facilities which may not have a phone and deal with all customers on their website.</p>
                 </div>
                 <ul className="grid gap-2 py-4">
@@ -208,8 +208,8 @@ export default function IndexPage() {
                     <li><Icons.check className="mr-2 inline-block h-4 w-4" /> Frontline support</li>
                 </ul>
             </div>
-            <div className="relative flex flex-col space-y-2 border px-3 py-3 rounded-md">
-                <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center text-white text-xl font-bold z-10">Coming Soon</div>
+            <div className="relative flex flex-col space-y-2 border px-3 py-3 rounded-md bg-gray-100">
+                <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center text-gray-500 text-xl font-bold z-10">Coming Soon</div>
                 <div className="space-y-2">
                     <h3 className="text-2xl font-bold tracking-tighter">{basicPlan.name}</h3>
                     <p className="text-2xl font-bold tracking-tighter">${basicPlan.price}</p>
@@ -226,8 +226,8 @@ export default function IndexPage() {
                     <li><Icons.check className="mr-2 inline-block h-4 w-4" /> 10 hours of calls included</li>
                 </ul>
             </div>
-            <div className="relative flex flex-col space-y-2 border px-3 py-3 rounded-md">
-                <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center text-white text-xl font-bold z-10">Coming Soon</div>
+            <div className="relative flex flex-col space-y-2 border px-3 py-3 rounded-md bg-gray-100">
+                <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center text-gray-600 text-xl font-bold z-10">Coming Soon</div>
                 <div className="space-y-2">
                     <h3 className="text-2xl font-bold tracking-tighter">{proPlan.name}</h3>
                     <p className="text-2xl font-bold tracking-tighter">${proPlan.price}</p>
