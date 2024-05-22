e9033e5fc68f43ff0abb7df2f0581b1b57a897c3
seb-AA <seb@asgeranalytics.com>
2024-05-22 13:18:00 -0600

    support chat

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -50,7 +50,7 @@ export function MainNav({ items, children }: MainNavProps) {
         className="flex items-center space-x-2 md:hidden"
         onClick={() => setShowMobileMenu(!showMobileMenu)}
       >
-        {showMobileMenu ? <Icons.close /> : <Icons.bot />}
+        {showMobileMenu ? <Icons.close /> : <Icons.bell />}
         <span className="font-bold">Menu</span>
       </button>
       {showMobileMenu && items && (
