3a6232d663a35070a914efd14213ec99a4c1f661
seb-AA <seb@asgeranalytics.com>
2024-05-25 16:54:24 -0600

    bye bye api 4.0

--- lib/validations/chatbot.ts
+++ lib/validations/chatbot.ts
@@ -2,10 +2,10 @@ import * as z from "zod"
 
 export const chatbotSchema = z.object({
     name: z.string().min(3).max(50),
-    openAIKey: z.string().min(1),
+    openAIKey: z.string().min(1).optional(),
     prompt: z.string().min(1),
     welcomeMessage: z.string().min(1),
     chatbotErrorMessage: z.string().min(1),
-    modelId: z.string().min(1),
+    modelId: z.string().min(1).optional(),
     files: z.array(z.string()),
 })
