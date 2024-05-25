af4de4a6d25ec89d72e17524b9a9ec75e03b473c
seb-AA <seb@asgeranalytics.com>
2024-05-25 10:02:18 -0600

    bye bye api 3.8

--- components/new-chatbot-form.tsx
+++ components/new-chatbot-form.tsx
@@ -40,7 +40,7 @@ export function NewChatbotForm({ isOnboarding, className, ...props }: NewChatbot
             prompt: "You are an assistant you help users that visit our website, keep it short, always refer to the documentation provided and never ask for more information.",
             chatbotErrorMessage: "Oops! An error has occurred. If the issue persists, feel free to reach out to our support team for assistance. We're here to help!",
             openAIKey: process.env.DEFAULT_CHATBOT_API_KEY,
-            modelId: process.env.DEFAULT_CHATBOT_MODEL
+            modelId: process.env.DEFAULT_CHATBOT_MODEL,
         }
     })
 
