28a32368d78a1873cf902ac0ac2be19fd90c6bc3
seb-AA <seb@asgeranalytics.com>
2024-06-10 15:13:21 -0600

    instant preview assistant avatar

--- app/(marketing)/roadmap/page.tsx
+++ app/(marketing)/roadmap/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from 'react';
 import futureFeatures from '../../../config/roadmap/futureFeatures.json';
 import integratedFeatures from '../../../config/roadmap/integratedFeatures.json';
